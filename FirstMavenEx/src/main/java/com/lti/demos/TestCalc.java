1ec75acab488fd700b641b5f516bbd1bec0c8fdb
Abhishekkumar11111 <abhishekkumar857885@gmail.com>
2022-03-14 16:59:43 +0530

    my first commit

---
+++ FirstMavenEx/src/main/java/com/lti/demos/TestCalc.java
@@ -0,0 +1,14 @@
+package com.lti.demos;
+
+public class TestCalc {
+	
+	public static void main(String[] args) {
+		
+		Calculator c=new Calculator();
+		int r=c.add(300, 200);
+		
+		System.out.println(r);
+		
+	}
+
+}
